b4c26b7118fc1c0747aee0c258c440094a72ea99
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-13 00:52:27 +0200

    03. usefull-things-393-portfolio. UI menu.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { Button } from './components/ui/button';
+
 function App() {
-  return <>start</>;
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 
+import Sidebar from '@/components/Sidebar.tsx';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className='lg:flex lg:justify-center lg:items-start lg:gap-10 min-h-screen'>
+      <Sidebar />
+      <App />
+    </div>
   </StrictMode>,
 );
