4b4ff6ea88033257a87ecf90d3af753f529bf531
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-11-13 22:45:33 +0200

    06. usefull-things-393-portfolio. UI Hero.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+
+import Hero from '@/components/Hero';
+
 function App() {
-  return <div className='h-[300vh]'></div>;
+  return (
+    <main className='flex flex-col container mx-auto p-10 max-w-4xl lg:pr-10 lg:pl-0 lg:max-w-6xl'>
+      <Hero />
+    </main>
+  );
 }
 
 export default App;
